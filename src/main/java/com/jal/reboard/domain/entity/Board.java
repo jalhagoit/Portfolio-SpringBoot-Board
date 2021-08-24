c1b7a762bfdbb434e096a681b398218ffe022ca6
jalhagoit <pregdyt@gmail.com>
2021-08-24 14:14:57 +0900

    글목록 조회 페이지에 생성일, 수정일 표기. (표기 형태 변경해야 함)

--- src/main/java/com/jal/reboard/domain/entity/Board.java
+++ src/main/java/com/jal/reboard/domain/entity/Board.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Builder
 @NoArgsConstructor
