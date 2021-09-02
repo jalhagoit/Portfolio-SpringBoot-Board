b01ad38271e42f05d05c9a2b37571d860cb3a536
jalhagoit <pregdyt@gmail.com>
2021-09-02 14:44:35 +0900

    .csrf().disable() 403 forbidden

--- src/main/java/com/jal/reboard/security/SecurityConfig.java
+++ src/main/java/com/jal/reboard/security/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .formLogin()
                 .loginPage("/login")
                 .failureUrl("/loginError")
